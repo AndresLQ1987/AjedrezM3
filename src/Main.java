340d587819911426e029c3b0acc4b065ba6b7972
AndresLQ1987 <andreslq1987.alq@gmail.com>
2019-04-20 08:14:06 -0700

    retocando main para pruebas tablero

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * 
- * @author andres,christian,jose,cayetano
+ * @author andres,christian,jose,catriel
  *
  */
 
 public class Main {
+	
+	public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	
 	public static void main(String[] args) {
 		
 		//crea el tablero y lo muestra por pantalla
 		Tablero tablero = new Tablero();
 		tablero.dibujaTablero();
 		
-		
+		boolean wellPos = false;
+		while(!wellPos) {
+			System.out.println("Introduce la posicion de la pieza(ej: 0 0): ");
+			try {
+				String pos = reader.readLine();
+				String[] splitpos = pos.split(" ");
+				try {
+					int piezaPosX = Integer.parseInt(splitpos[0]);
+				}catch (Exception e) {
+					// TODO: handle exception
+				}
+			}catch (Exception e) {
+				System.out.println("Error posicion en Blanco.");
+			}
+		}
 	}
 }
