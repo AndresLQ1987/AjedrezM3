cecc00449d84284bd8d9647c002f1c949c244daf
Andres Lopez <cf18andres.lopez@iesjoandaustria.org>
2019-04-24 19:05:23 +0200

    cambiado crear tablero de string a piezas

--- src/Tablero.java
+++ src/Tablero.java
@@ -10,54 +10,54 @@ public class Tablero {
 	
 	private int numCasillas;
 	//Tablero que guarda la posicion de las piezas
-	private String[][] tablero; //temporalmente tablero de strings cambiar por figura mas adelante.
+	private Piezas[][] tablero; //temporalmente tablero de strings cambiar por figura mas adelante.
 	
 	//Constructor Basico que inicia las piezas en su posicion
-	Tablero(){
+	public Tablero(){
 		numCasillas = 8;
-		tablero = new String[numCasillas][numCasillas];
+		tablero = new Piezas[numCasillas][numCasillas];
 		for(int y = 0 ; y < numCasillas ; y++) {
 			for(int x = 0 ; x < numCasillas ; x++) {
 				if(y == 1 || y == 6) { //Este if coloca los peones
 					if(y == 1) {
-						tablero[x][y] = "PN"; // temporalmente son String cambiar por la pieza que toque una vez esten.
+						tablero[x][y] = new Piezas(x,y,"N","P"); //new Peon(x,y,"N","P"); // temporalmente son String cambiar por la pieza que toque una vez esten.
 					}else {
-						tablero[x][y] = "PB";
+						tablero[x][y] = new Piezas(x,y,"B","P");//new Peon(x,y,"B","P");
 					}
 				}else if(y == 0 || y == 7) {
 					if(x == 0 || x == 7) { // coloca las torres
 						if(y == 0) {
-							tablero[x][y] = "TN";
+							tablero[x][y] = new Piezas(x,y,"N","T");
 						}else {
-							tablero[x][y] = "TB";
+							tablero[x][y] = new Piezas(x,y,"B","T");
 						}
 					}else if(x == 1 || x == 6) { // coloca los caballos
 						if(y == 0) {
-							tablero[x][y] = "CN";
+							tablero[x][y] = new Piezas(x,y,"N","C");
 						}else {
-							tablero[x][y] = "CB";
+							tablero[x][y] = new Piezas(x,y,"B","C");
 						}
 					}else if(x == 2 || x == 5) { // coloca los alfiles
 						if(y == 0) {
-							tablero[x][y] = "AN";
+							tablero[x][y] = new Piezas(x,y,"N","A");
 						}else {
-							tablero[x][y] = "AB";
+							tablero[x][y] = new Piezas(x,y,"B","A");
 						}
 					}else if(x == 3) { // coloca la dama (o Reina)
 						if(y == 0) {
-							tablero[x][y] = "DN";
+							tablero[x][y] = new Piezas(x,y,"N","D");
 						}else {
-							tablero[x][y] = "DB";
+							tablero[x][y] = new Piezas(x,y,"B","D");
 						}
 					}else { // coloca el rey
 						if(y == 0) {
-							tablero[x][y] = "RN";
+							tablero[x][y] = new Piezas(x,y,"N","R");
 						}else {
-							tablero[x][y] = "RB";
+							tablero[x][y] = new Piezas(x,y,"B","R");
 						}
 					}
 				}else {
-					tablero[x][y] = "  ";
+					tablero[x][y] = new Piezas(x,y," "," ");
 				}
 			}
 		}
@@ -95,28 +95,7 @@ public class Tablero {
 	
 	//funcion que pregunta los movimientos de la pieza en las posiciones X y Y
 	public boolean[][] pregMovimientos(int x, int y){ //modificar cuando esten las piezas
-		boolean [][] result = new boolean[numCasillas][numCasillas];
-		if(tablero[x][y] == "PB") {
-			result = movPB(x,y);
-			return result;
-		}
+		boolean [][] result = tablero[x][y].movimiento(x, y,tablero[x][y].getColor());
 		return result;
-	}
-	
-	//funcion de prueba pregMovimiento
-	private boolean[][] movPB(int x, int y){
-		boolean[][] movimientos = new boolean[numCasillas][numCasillas];
-		for(int a = 0 ; a < numCasillas ; a++) {
-			for(int b = 0 ; b < numCasillas ; b++) {
-				if(b == x && a == y-1) {
-					if(y-1 >= 0) {
-						movimientos[a][b] = true; 
-					}
-				}else {
-					movimientos[a][b] = false;
-				}
-			}
-		}
-		return movimientos;
 	}
 }
