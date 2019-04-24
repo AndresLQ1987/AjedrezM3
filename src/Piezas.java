f5c36b6513c03186d093368cef4faf2a0d4060bc
Christian Villegas <cf18christian.villegas@iesjoandaustria.org>
2019-04-24 17:24:48 +0200

    Añadido class Piezas

---
+++ src/Piezas.java
@@ -0,0 +1,32 @@
+public class Piezas {
+	public int x;
+	public int y;
+	public String color;
+	
+	public Piezas (int x, int y, String color) {
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
+	
+    public int getX(){
+        return x;
+    }
+    public void setX(int x){
+
+    }
+    
+    public int getY() {
+    	return y;
+    }
+    public void setY(int y) {
+    	
+    }
+    public String getColor() {
+    	return color;
+    }
+    public void setColor(String color) {
+    	
+    }
+}
+	
