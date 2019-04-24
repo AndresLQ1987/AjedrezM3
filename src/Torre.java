dde4a101027100ed9ed3c0752db830736e1b2e92
ImCTRL <catriel.jhonny.molina@gmail.com>
2019-04-24 19:06:16 +0200

    He reiniciado el movimiento de la pieza torre.

---
+++ src/Torre.java
@@ -0,0 +1,19 @@
+public class torre extends Piezas {
+
+    public torre(int x, int y, String color, String nombre) {
+        super(x, y, color, nombre);
+    }
+
+    public void Movimiento(int movimiento_desdeX, int movimiento_desdeY) {
+
+        int desdeX = movimiento_desdeX;
+        int desdeY = movimiento_desdeY;
+    }
+
+    String ruta;
+
+    if(desdeY=0){
+    //continuar desde aqui.
+    }
+}
+
