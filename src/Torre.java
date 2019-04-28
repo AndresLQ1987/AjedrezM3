39cc3deea45c1c802dad0d44427aa8e0fb6cabbc
ImCTRL <catriel.jhonny.molina@gmail.com>
2019-04-28 13:33:55 +0200

    En principio esta la torre preparada para hacer las pruebas.

--- src/Torre.java
+++ src/Torre.java
@@ -6,6 +6,12 @@ Incluye bucle de verificación de posiciones para poder enseñar posibles movimi
  dado que en un principio quería hacer una cadena de comprobación basada en IF's.
  */
 public class Torre extends Piezas {
+
+    //Este es el constructor de la pieza Torre.
+    public Torre (int x, int y, String color, String nombre) {
+        super(x, y, color, nombre);
+    }
+
     /*
     Se crea una matriz, la cual recibira 2 enteros y un string especificando el color.
     Finalmente este retornara la comprobación en forma de true/false de la posición
@@ -14,8 +20,8 @@ public class Torre extends Piezas {
     public boolean[][] movimiento(int x, int y) {
         boolean[][] movimiento = new boolean[8][8]; //Se crea una matriz para la posición.
 
-        for(int i=0; i=8; i=i+1) {
-            for(int j=0; j=8; j=j+1) {
+        for(int i=0; i<8; i=i+1) {
+            for(int j=0; j<8; j=j+1) {
                 if ((j == x) || (i == y)) {
                     movimiento[i][j] = true; //Verificación para determinar si el movimiento es correcto.
 
