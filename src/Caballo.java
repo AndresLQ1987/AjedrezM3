405f99a7aa958beb0e39e90c1f0c611f6218ad92
ImCTRL <catriel.jhonny.molina@gmail.com>
2019-04-29 20:29:30 +0200

    Caballito actualizado.

--- src/Caballo.java
+++ src/Caballo.java
@@ -6,17 +6,15 @@
 * by Catriel
  */
 public class Caballo extends Piezas {
-
     /*
     *Aqui tenemos el constructor del caballito.
      */
     public Caballo(int x, int y, String color, String nombre) {
         super(x, y, color, nombre);
     }
-
     /*
     *Aqui comenzamos la matriz el cual delimitara los bordes de los movimientos.
-     */
+    */
     @Override
     public boolean[][] movimiento(int x, int y, String color) {
         boolean[][] movimiento = new boolean[8][8];
@@ -27,47 +25,71 @@ public class Caballo extends Piezas {
         * Dentro hay un if que se acticara con cada comprobación, devolviendo al final
         * tan solo los movimientos que no se salgan de los limites.
          */
-        for (int i=0; i < 8; i=i+1) {
 
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<8; j++) {
+                if ((i != x) || (j != y)) {
+                    movimiento[i][j] = false; //Verificación para determinar si el movimiento es correcto.
+                }
+            }
+        }
+
+        for (int i=0; i < 8; i=i+1) {
             if (i == 0) { // movimiento: arriba derecha
-                y = y + 2;
-                x = x + 1;
-                movimiento[y][x] = (x >= 0) && (x <= 7) && (y >= 0) && (y <= 7);
+                int y2 = y + 2;
+                int x2 = x + 1;
+                if ((x2 >= 0) && (x2 <= 7) && (y2 >= 0) && (y2 <= 7)) {
+                    movimiento[y][x] = true;
+                }
             }
             if (i == 1) { //movimiento: arriba a la izquierda
-                y = y + 2;
-                x = x - 1;
-                movimiento[y][x] = (x >= 0) && (x <= 7) && (y >= 0) && (y <= 7);
+                int y2 = y + 2;
+                int x2 = x - 1;
+                if ((x2 >= 0) && (x2 <= 7) && (y2 >= 0) && (y2 <= 7)) {
+                    movimiento[y][x] = true;
+                }
             }
             if (i == 2) { //movimiento: derecha arriba
-                y = y + 1;
-                x = x + 2;
-                movimiento[y][x] = (x >= 0) && (x <= 7) && (y >= 0) && (y <= 7);
+                int y2 = y + 1;
+                int x2 = x + 2;
+                if ((x2 >= 0) && (x2 <= 7) && (y2 >= 0) && (y2 <= 7)) {
+                    movimiento[y][x] = true;
+                }
             }
             if (i == 3) { //movimiento: derecha abajo
-                y = y - 1;
-                x = x + 2;
-                movimiento[y][x] = (x >= 0) && (x <= 7) && (y >= 0) && (y <= 7);
+                int y2 = y - 1;
+                int x2 = x + 2;
+                if ((x2 >= 0) && (x2 <= 7) && (y2 >= 0) && (y2 <= 7)) {
+                    movimiento[y][x] = true;
+                }
             }
             if (i == 4) { //movimiento: abajo derecha
-                y = y - 2;
-                x = x + 1;
-                movimiento[y][x] = (x >= 0) && (x <= 7) && (y >= 0) && (y <= 7);
+                int y2 = y - 2;
+                int x2 = x + 1;
+                if ((x2 >= 0) && (x2 <= 7) && (y2 >= 0) && (y2 <= 7)) {
+                    movimiento[y][x] = true;
+                }
             }
             if (i == 5) { //movimiento abajo izquierda
-                y = y - 2;
-                x = x - 1;
-                movimiento[y][x] = (x >= 0) && (x <= 7) && (y >= 0) && (y <= 7);
+                int y2 = y - 2;
+                int x2 = x - 1;
+                if ((x2 >= 0) && (x2 <= 7) && (y2 >= 0) && (y2 <= 7)) {
+                    movimiento[y][x] = true;
+                }
             }
             if (i == 6) { //izquierda arriba
-                y = y + 1;
-                x = x - 2;
-                movimiento[y][x] = (x >= 0) && (x <= 7) && (y >= 0) && (y <= 7);
+                int y2 = y + 1;
+                int x2 = x - 2;
+                if ((x2 >= 0) && (x2 <= 7) && (y2 >= 0) && (y2 <= 7)) {
+                    movimiento[y][x] = true;
+                }
             }
             if (i == 7) { //izquierda abajo
-                y = y - 1;
-                x = x - 2;
-                movimiento[y][x] = (x >= 0) && (x <= 7) && (y >= 0) && (y <= 7);
+                int y2 = y - 1;
+                int x2 = x - 2;
+                if ((x2 >= 0) && (x2 <= 7) && (y2 >= 0) && (y2 <= 7)) {
+                    movimiento[y][x] = true;
+                }
             }      
         }
       //Aqui es donde devolvemos la matriz de posiciones posibles.
